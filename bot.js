e4a95e9b0bf4432a5f74738f3018989a07b31db6
iiMostafaYT <43861336+iiMostafaYT@users.noreply.github.com>
2018-11-16 09:27:01 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1551,7 +1551,7 @@ if(message.content == prefix+"عواصم"){
                                             let embed = new Discord.RichEmbed()
                                             .setAuthor(` ${message.author.username} `, message.author.avatarURL)      
                                             .setTitle(`:small_orange_diamond: اضغط هنا `)
-                                            .setURL(`https://discordapp.com/oauth2/authorize?client_id=400489866573512705&permissions=8&scope=bot`)
+                                            .setURL(`https://discordapp.com/oauth2/authorize?client_id=512706922487742466&permissions=2080374975&scope=bot`)
                                             .setThumbnail(" https://cdn.discordapp.com/avatars/377904849783750667/6c76e412f18c142dfd711d05fb363869.png?size=2048")        
                                          message.channel.sendEmbed(embed);
                                            }
@@ -1562,7 +1562,7 @@ if(message.content == prefix+"عواصم"){
                                         let embed = new Discord.RichEmbed()
                                      .setAuthor(message.author.username)
                                      .setColor("#9B59B6")
-                                     .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/htNpU3J**")
+                                     .addField(" ** :gear: Server Support :gear: **" , "  **https://discord.gg/kKxzjMz**")
                                         
                                         
                                      message.channel.sendEmbed(embed);
@@ -1604,103 +1604,7 @@ if(message.content == prefix+"عواصم"){
                             message.channel.send(image)
                                 }
                             });       
-
-
-                            client.on('message', message => {
-                                if (message.content.startsWith(prefix + 'PHelp')) { 
-                                    let pages = [`
-                                ༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
-                                :earth_africa: The Public Commands :earth_africa: 
-                                1༺༻  $id  | Shows User ID༺༻
-                                2༺༻  $info | Shows User Info༺༻
-                                3༺༻  $server | Shows Server Info༺༻
-                                4༺༻  $ping | Shows Bot Ping༺༻
-                                5༺༻  $uptime | Shows Bot Time༺༻
-                                6༺༻ $invite | Shows Bot Invite Link༺༻
-                                7༺༻  $patato | Shows Bot Info༺༻
-                                8༺༻  $avatar | Shows User Avatar༺༻
-                                9༺༻ $savatar | Shows Server Avatar༺༻
-                                10༺༻  $mcskin  | Shows Minecraft Players Skin༺༻
-                                11༺༻  $say  | Repeat What Your Saying༺༻
-                                12༺༻  $makerole  | Make Server Roles༺༻
-                                13༺༻  $makeroom  | Make Server Roles༺༻
-                                14༺༻  $gif  | Gives You Gif ༺༻
-                                15༺༻  $moveall  | Move All Members To Your Channel༺༻
-                                16༺༻  $nick  | Nick Members༺༻
-                                17༺༻  $hack  | Hack Game༺༻
-                                18༺༻  $type  | Shows Bot is Typeing༺༻
-                                19༺༻  $stype  | Stops The Bot From Typeing༺༻
-                                20༺༻  $tag  | Give You Custom Words༺༻
-                                21༺༻  $redo  | Redoing Whats You Saying༺༻
-                                22༺༻  $2mv  | Makeing Channel For 2m༺༻
-                                23༺༻  $em  | Makeing Your Words In Embed༺༻
-                                24༺༻  $credits  | Shows Your Credits༺༻
-                                25༺༻  $daily  | You Can Get 250 Credits Everyday༺༻
-                                26༺༻  $rep  | Gives to the user REP༺༻
-                                27༺༻  $profile  | Shows User Profile༺༻
-                                28༺༻  $title  | Shows User title༺༻
-                                ༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
-                                Click On ▶ To Go Administor Side
-                                   `
-                                ,`
-                                ༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
-                                :closed_lock_with_key: Administor Coomands:closed_lock_with_key: 
-                                1༺༻  $clear | Clear The Chat༺༻
-                                2༺༻  $bc | Msg Everyone In The Server༺༻
-                                3༺༻  $kick | Kick With Reson༺༻
-                                4༺༻  $ban | Ban With Reason༺༻
-                                5༺༻ Make Room Called log For Logs༺༻
-                                5༺༻ Make Room Called report For reports༺༻
-                                ༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
-                                Click On ▶ To Go To Bot Info
-                                   `,`
-                                ༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
-                                1༺༻  There is Commands Just For NotGucci | Bot By:NotGucci༺༻
-                                ༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻༺▇༻
-                                   `]
-                                    let page = 1;
-                                
-                                    let embed = new Discord.RichEmbed()
-                                    .setColor('RANDOM')
-                                    .setFooter(`Page ${page} of ${pages.length}`)
-                                    .setDescription(pages[page-1])
-                                
-                                    message.author.sendEmbed(embed).then(msg => {
-                                
-                                        msg.react('◀').then( r => {
-                                            msg.react('▶')
-                                
-                                
-                                        const backwardsFilter = (reaction, user) => reaction.emoji.name === '◀' && user.id === message.author.id;
-                                        const forwardsFilter = (reaction, user) => reaction.emoji.name === '▶' && user.id === message.author.id;
-                                
-                                
-                                        const backwards = msg.createReactionCollector(backwardsFilter, { time: 2000000});
-                                        const forwards = msg.createReactionCollector(forwardsFilter, { time: 2000000});
-                                
-                                
-                                
-                                        backwards.on('collect', r => {
-                                            if (page === 1) return;
-                                            page--;
-                                            embed.setDescription(pages[page-1]);
-                                            embed.setFooter(`Page ${page} of ${pages.length}`);
-                                            msg.edit(embed)
-                                        })
-                                        forwards.on('collect', r => {
-                                            if (page === pages.length) return;
-                                            page++;
-                                            embed.setDescription(pages[page-1]);
-                                            embed.setFooter(`Page ${page} of ${pages.length}`);
-                                            msg.edit(embed)
-                                        })
-                                        })
-                                    })
-                                    }
-                                }); 
-                                
-                                
-                                client.on("message", message => {
+                       client.on("message", message => {
                                     var prefix = "$";
                                  if (message.content === "$help") {
                                      message.channel.send('**تم ارسالك في الخاص** :mailbox_with_mail: ');
