2b806373711fb8c87c887c42b110c2429e4f6bc2
iiMostafaYT <43861336+iiMostafaYT@users.noreply.github.com>
2018-11-17 10:32:29 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1437,7 +1437,7 @@ client.on('message',async message => {
   }
   
     var filter = m => m.author.id === message.author.id;
-    if(message.content.startsWith("!giveaway")) {
+    if(message.content.startsWith("$giveaway")) {
   
       if(!message.guild.member(message.author).hasPermission('MANAGE_GUILD')) return message.channel.send(':heavy_multiplication_x:| **يجب أن يكون لديك خاصية التعديل على السيرفر**');
       message.channel.send(`:eight_pointed_black_star:| **Send Name channel For the Giveaway**`).then(msg => {
