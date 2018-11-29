e0c7625751cb1a55c3553b6bd20923b55364e5cd
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-11-29 18:15:36 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -639,10 +639,62 @@ client.on('message', message => { //By |.iiMostafaYT#1001
             .addField('**Platform**' , `[ ${process.platform} ]` , true) //By |.iiMostafaYT#1001
                   .addField('**My Prefix**' , `[ ${prefix} ]` , true) //By |.iiMostafaYT#1001
                   .addField('**My Language**' , `[ Java Script ]` , true) //By |.iiMostafaYT#1001
-                  .setFooter('By | .iiMosTaFaYT#1001') //By |.iiMostafaYT#1001
+                  .setFooter('By | @《SK》| HeemPlayz#6784 ') //By |.iiMostafaYT#1001
     }) //By |.iiMostafaYT#1001
 } //By |.iiMostafaYT#1001
 }); //By |.iiMostafaYT#1001
+
+client.on('message', message => {
+     var prefix = "$"
+if (message.content.startsWith(prefix + "uptime")) {
+    let uptime = client.uptime;
+ 
+    let days = 0;
+    let hours = 0;
+    let minutes = 0;
+    let seconds = 0;
+    let notCompleted = true;
+ 
+    while (notCompleted) {
+ 
+        if (uptime >= 8.64e+7) {
+ 
+            days++;
+            uptime -= 8.64e+7;
+ 
+        } else if (uptime >= 3.6e+6) {
+ 
+            hours++;
+            uptime -= 3.6e+6;
+ 
+        } else if (uptime >= 60000) {
+ 
+            minutes++;
+            uptime -= 60000;
+ 
+        } else if (uptime >= 1000) {
+            seconds++;
+            uptime -= 1000;
+ 
+        }
+ 
+        if (uptime < 1000)  notCompleted = false;
+ 
+    }
+ 
+    message.channel.send("`" + `${days} days, ${hours} hrs, ${minutes} min , ${seconds} sec` + "`");
+ 
+ 
+}
+});
+
+client.on('message', message=> {
+    if (message.author.bot) return;
+    if (message.isMentioned(client.user))
+    {
+    message.reply("Hey!\nTo Get Started\n`$help`\nBot Developer\n`《SK》| HeemPlayz#6784`\nMy Prefix:\n`$`");
+    }
+});
 
 
  
