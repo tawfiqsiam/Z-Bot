08ae494123c97ca93c23ff2bb740daa8432f9657
The Best Bot Ever <42186537+HeemPlayz@users.noreply.github.com>
2018-11-29 17:29:36 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1760,7 +1760,7 @@ const log = JSON.parse(fs.readFileSync('./log.json' , 'utf8'));
 //Perfect log Code
 client.on('message', message => {
     let room = message.content.split(" ").slice(1);
-    let findroom = message.guild.channels.find('name', `${room}`)
+    let findroom = message.guild.channels.find('name', `${room}`);
     if(message.content.startsWith("$setLog")) {
         if(!message.channel.guild) return message.reply('**This Command Only For Servers**');
         if(!message.member.hasPermission('MANAGE_GUILD')) return message.channel.send('**Sorry But You Dont Have Permission** `MANAGE_GUILD`' );
