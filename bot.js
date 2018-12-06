16c798d53c03e550f938fece11fa700809c8d1ee
Heem <42186537+HeemPlayz@users.noreply.github.com>
2018-12-06 19:08:37 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4186,57 +4186,6 @@ collector7.on('collect', r => {
 }
 });
 
-const credits = JSON.parse(fs.readFileSync("./creditsCode.json", "utf8"));
-
-const coolDown = new Set();
-
-client.on('message',async message => {
-    
-if(message.author.bot) return;
-if(!credits[message.author.id]) credits[message.author.id] = {
-    credits: 50
-};
-
-let userData = credits[message.author.id];
-let m = userData.credits;
-
-fs.writeFile("./creditsCode.json", JSON.stringify(credits), (err) => {
-    if (err) console.error(err);
-  });
-  credits[message.author.id] = {
-      credits: m + 0.5,
-  }
-  
-    if(message.content.startsWith(prefix + "credit" || prefix + "credits")) {
-message.channel.send(`**${message.author.username}, your :credit_card: balance is \`\`${userData.credits}\`\`.**`);
-}
-});
-
-client.on('message', async message => {
-    let amount = 250 || 302;
-    if(message.content.startsWith(prefix + "daily")) {
-    if(message.author.bot) return;
-    if(coolDown.has(message.author.id)) return message.channel.send(`**:stopwatch: | ${message.author.username}, your daily :yen: credits refreshes in \`\`1 Day\`\`.**`);
-    
-    let userData = credits[message.author.id];
-    let m = userData.credits + amount;
-    credits[message.author.id] = {
-    credits: m
-    };
-
-    fs.writeFile("./creditsCode.json", JSON.stringify(userData.credits + amount), (err) => {
-    if (err) console.error(err);
-    });
-    
-    message.channel.send(`**:atm: | ${message.author.username}, you received your :yen: ${amount} credits!**`).then(() => {
-        coolDown.add(message.author.id);
-    });
-    
-    setTimeout(() => {
-       coolDown.remove(message.author.id);
-    },86400000);
-    }
-});
 
 
                client.on('message', message => {                                         var prefix ="$";                                               if (message.content.startsWith(prefix + "user")) {                                         var args = message.content.split(" ").slice(1);                                         let user = message.mentions.users.first();                                         var men = message.mentions.users.first();                                            var heg;                                            if(men) {                                                heg = men                                            } else {                                                heg = message.author                                            }                                          var mentionned = message.mentions.members.first();                                             var h;                                            if(mentionned) {                                                h = mentionned                                            } else {                                                h = message.member                                            }                                                   moment.locale('ar-TN');                                          var id = new  Discord.RichEmbed()                                          .setAuthor(message.author.username, message.author.avatarURL)                                         .setColor("#707070")                                        .addField(': دخولك لديسكورد قبل', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true)                                         .addField(': انضمامك لسيرفر قبل', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)                                                       .setFooter(`SkyBot.`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')                                                                         .setThumbnail(heg.avatarURL);                                        message.channel.send(id)                                    }       });                                                                   
