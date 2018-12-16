2cc37be55177bba0c300a13400c73fb944139d29
Heem <42186537+HeemPlayz@users.noreply.github.com>
2018-12-16 11:49:51 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -4580,15 +4580,59 @@ client.on('voiceStateUpdate', (voiceOld, voiceNew) => {
     }
 });
 
-client.on('message', msg => {//msg
-    if (msg.content === `${prefix}colors` || 'الوان') {
+client.on('message', msg => {
+    if (msg.content === `${prefix}colors`) {
+      msg.channel.send({file : "https://cdn.discordapp.com/attachments/501774006966419458/501774646467887105/colors.png"})
+    }
+  });;
+
+client.on('message', msg => {
+    if (msg.content === 'الوان') {
       msg.channel.send({file : "https://cdn.discordapp.com/attachments/501774006966419458/501774646467887105/colors.png"})
     }
   });;
  
   client.on('message', message => {
     let args = message.content.split(' ').slice(1);
-    if(message.content.split(' ')[0] == `${prefix}color` || 'لون') {
+    if(message.content.split(' ')[0] == `${prefix}color`) {
+    const embedd = new Discord.RichEmbed()
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+    .setDescription(`**لا يوجد لون بهذا الأسم ** :x: `)
+    .setColor(`ff0000`)
+   
+    if(!isNaN(args) && args.length > 0)
+   
+   
+    if    (!(message.guild.roles.find("name",`${args}`))) return  message.channel.sendEmbed(embedd);
+   
+   
+    var a = message.guild.roles.find("name",`${args}`)
+     if(!a)return;
+    const embed = new Discord.RichEmbed()
+   
+    .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+    .setDescription(`**Done , تم تغير لونك . :white_check_mark: **`)
+   
+    .setColor(`${a.hexColor}`)
+    message.channel.sendEmbed(embed);
+    if (!args)return;
+    setInterval(function(){})
+       let count = 0;
+       let ecount = 0;
+    for(let x = 1; x < 201; x++){
+   
+    message.member.removeRole(message.guild.roles.find("name",`${x}`))
+   
+    }
+     message.member.addRole(message.guild.roles.find("name",`${args}`));
+   
+   
+    }
+    });
+
+client.on('message', message => {
+    let args = message.content.split(' ').slice(1);
+    if(message.content.split(' ')[0] == 'لون') {
     const embedd = new Discord.RichEmbed()
     .setFooter('Requested by '+message.author.username, message.author.avatarURL)
     .setDescription(`**لا يوجد لون بهذا الأسم ** :x: `)
